a37fc7103d64d5a4b183b48033789d24c59afa4e
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-05-30 12:44:41 +0200

    Hello Worlds

---
+++ libft/norm.sh
@@ -0,0 +1,17 @@
+echo Hello user, please allow me to access your Libft so I can consume and copy all the files to my private... I mean Norme your files...
+
+dir=./
+head=./
+
+for file in $src*c
+do
+	echo copying and normining $file
+	norminette $file
+	sleep .01	
+done
+for file in $head*.h
+do
+	echo copying and norming $file
+	norminette $file
+	sleep .01
+done
