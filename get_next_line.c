ca6078545b55cc94a961477a8ebb722d6ed7f4c7
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-05-30 18:18:25 +0200

    FUCK !

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,93 @@
 /*   By: jwolf <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 16:10:54 by jwolf             #+#    #+#             */
-/*   Updated: 2018/05/30 08:58:43 by jwolf            ###   ########.fr       */
+/*   Updated: 2018/05/30 18:16:30 by jwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft/libft.h"
 
-int		get_next_line(const int fd, char **line)
+static char		*joinchr(char *s1, char c)
 {
-	return (0);
+	char	*new;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !c)
+		return (NULL);
+	j = ft_strlen(s1);
+	new = ft_strnew(j + 1);
+	if (!new)
+		return (NULL);
+	i = -1;
+	while (++i < j)
+		*(new + i) = *(s1 + i);
+	*(new + i) = c;
+	return (new);
+}
+
+static	int		contentcopy(char **dst, char *src, char c)
+{
+	int		i;
+	int		pos;
+	int		cc;
+
+	i = -1;
+	cc = 0;
+	while (src[++i])
+		if (src[i] == c)
+			break ;
+	pos = i;
+	if (!(*dst = ft_strnew(i)))
+		return (0);
+	while (src[cc] && cc < i)
+	{
+		if (!(*dst = joinchr(*dst, src[cc])))
+			return (0);
+		cc++;
+	}
+	return (pos);
+}
+
+static t_list	*get_file(t_list **file, int fd)
+{
+	t_list	*tmp;
+	
+	tmp = *file;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("\0", fd);
+	ft_lstadd(file, tmp);
+	tmp = *file;
+	return (tmp);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	char			buf[BUFF_SIZE + 1];
+	int				ret;
+	int				i;
+	static t_list	*files;
+	t_list			*c_file;
+
+	if (fd < 0 || !line)
+		return (0);
+	c_file = get_file(&files, fd);
+	MALLCHECK((*line = ft_strnew(1)));
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		MALLCHECK((c_file->content = ft_strjoin(c_file->content, buf)));
+		if (ft_strrchr(buf, '\n'))
+				break ;
+	}
+	i = contentcopy(line, c_file->content, '\n');
+	(i < (int)ft_strlen(c_file->content)) ? c_file->content += (i + 1) :
+		ft_strclr(c_file->content);
+	return (1);	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: jwolf <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 16:11:10 by jwolf             #+#    #+#             */
-/*   Updated: 2018/05/28 16:16:02 by jwolf            ###   ########.fr       */
+/*   Updated: 2018/05/30 18:13:11 by jwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
-#define BUFF_SIZE 32
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE		32	
+# define MALLCHECK(x)	if (!x) return (-1)
 
 # include <unistd.h>
 # include <fcntl.h>
