208986ed0dec6f562bd6f681e408b89f57da5e65
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-06-06 08:52:08 +0200

    Stuff Works

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: jwolf <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 16:11:10 by jwolf             #+#    #+#             */
-/*   Updated: 2018/06/05 14:42:13 by jwolf            ###   ########.fr       */
+/*   Updated: 2018/06/06 08:48:02 by jwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE	    32
+# define BUFF_SIZE	   	32 
 # define FD_MAX			50	
 
 # include <unistd.h>
